29bca4518e2e83085bb6a4cc0458bfafdb38367a
khaingwyuthmone <kwh.qwerty@gmail.com>
2024-09-16 22:24:00 +0900

    vault-cluster terraform outputfile added

--- vault-cluster/main.tf
+++ vault-cluster/main.tf
@@ -1,13 +1,13 @@
-resource "hcp_hvn" "example" {
+resource "hcp_hvn" "vault_hvn" {
   hvn_id         = "cohort7-hvn"
   cloud_provider = "aws"
   region         = "ap-southeast-1"
   cidr_block     = "172.25.16.0/20"
 }
 
-resource "hcp_vault_cluster" "example" {
+resource "hcp_vault_cluster" "vault_cluster" {
   cluster_id = "cohort7-vault-cluster"
-  hvn_id     = hcp_hvn.example.hvn_id
+  hvn_id     = hcp_hvn.vault_hvn.hvn_id
   tier       = "starter_small"
   public_endpoint = true
 }
