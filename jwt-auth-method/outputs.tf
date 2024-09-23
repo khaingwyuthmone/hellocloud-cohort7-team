2004ca7c47c689c0036ec5e28942043e2f2458c0
khaingwyuthmone <kwh.qwerty@gmail.com>
2024-09-23 12:12:00 +0900

    jwt-auth-method dir

---
+++ jwt-auth-method/outputs.tf
@@ -0,0 +1,3 @@
+output "role-name" {
+  value = vault_jwt_auth_backend_role.admin-role.role_name
+}
---
+++ jwt-auth-method/variables.tf
@@ -0,0 +1,5 @@
+variable "tfc_aws_secret_role_name" {
+  type = string
+  default = "admin-role"
+}
+
