37c3923d0cb50918b0d80b26670d4f134ae5f668
khaingwyuthmone <kwh.qwerty@gmail.com>
2024-10-01 21:32:03 +0900

    update

--- aws-secret-engine-day3/main.tf
+++ aws-secret-engine-day3/main.tf
@@ -20,8 +20,8 @@ resource "vault_aws_secret_backend_role" "role" {
   policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 }
 
-# Fetch AWS access credentials for the defined role from Vault
-data "vault_aws_access_credentials" "creds" {
-  backend = vault_aws_secret_backend.aws.path
-  role    = vault_aws_secret_backend_role.role.name
-}
+# # Fetch AWS access credentials for the defined role from Vault
+# data "vault_aws_access_credentials" "creds" {
+#   backend = vault_aws_secret_backend.aws.path
+#   role    = vault_aws_secret_backend_role.role.name
+# }
