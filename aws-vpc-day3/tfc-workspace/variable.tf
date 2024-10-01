45f6f0c9f52987619cb6498f33cf248cb433e301
khaingwyuthmone <kwh.qwerty@gmail.com>
2024-10-01 21:57:23 +0900

    format

--- aws-vpc-day3/tfc-workspace/variable.tf
+++ aws-vpc-day3/tfc-workspace/variable.tf
@@ -1,25 +1,25 @@
 variable "workspace_name" {
   description = "Workspace Name"
-  type = string
-  default = "aws-vpc-day3"
+  type        = string
+  default     = "aws-vpc-day3"
 }
 variable "org_name" {
   description = "Organization Name"
-  type = string
-  default = "hellocloud-cohort7-team"
+  type        = string
+  default     = "hellocloud_khaingwyuthmone"
 }
 variable "vault_url" {
   description = "The address of the Vault instance runs will access."
-  type = string
-  default = "https://cohort7-vault-cluster-public-vault-dade1026.a8ab6f61.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
+  type        = string
+  default     = "https://cohort7-vault-cluster-public-vault-54ffbcbe.b0f8ba19.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
 }
 variable "run_role" {
   description = "TFC_VAULT_RUN_ROLE"
-  type = string
-  default = "admin-role"
+  type        = string
+  default     = "admin-role"
 }
 variable "vault_namespace" {
   description = "TFC_VAULT_NAMESPACE"
-  type = string
-  default = "admin"
+  type        = string
+  default     = "admin"
 }
